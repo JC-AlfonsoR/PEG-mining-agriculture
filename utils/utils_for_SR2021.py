2a44ea557e7e951a527eda50587e2dd3479da1c2
J. Camilo Alfonso R. <jc.alfonso2004@gmail.com>
2026-07-15 17:33:20 -0500

    Agregar información para decodificación de minerales SR2021

---
+++ utils/utils_for_SR2021.py
@@ -0,0 +1,19 @@
+# utils_for_SR2021.py
+
+# En este script guardo información que uso para trabajar con las bases de datos de Saavedra & Romero 2021
+
+
+# Inferí la condificación de los minerales a partir de este archivo: SR_2021\RCode\MiningPredictions\src\municipioResultsMi.R (líneas 32-42)
+SR2021_mineral_code = {
+	1:	'gold',
+	2:	'platinum',
+	3:	'copper',
+	4:	'coal',
+	5:	'columbite',
+	6:	'iron',
+	7:	'magnesium',
+	8:	'nickel',
+	9:	'phosphate',
+	10:	'potassium',
+	11:	'uranium'
+}
