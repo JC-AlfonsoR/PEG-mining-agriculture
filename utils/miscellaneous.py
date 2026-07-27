2edca7f99b4fe216face3062296b8f4e1ba460df
J. Camilo Alfonso R. <jc.alfonso2004@gmail.com>
2026-07-27 18:21:57 -0500

    Completar armonización de categorías minerales

--- utils/miscellaneous.py
+++ utils/miscellaneous.py
@@ -2,3 +2,27 @@
 
 # Variables que uso frecuentemente y prefiero definir solo una vez
 MSC_SEPARADOR = "\n" + "-"*32 # Nueva línea y guiones para separar visualmente los mensajes que imprimo
+
+def MSC_normalizar_texto(texto):
+    
+    import unicodedata # aplanar caracteres especiales
+    import re # Expresiones regulares
+    import pandas as pd
+    
+    if pd.isna(texto):
+        return ""
+
+    # convertir todo a minusculas
+    texto = str(texto).lower()
+
+    # Eliminar tildes
+    texto = unicodedata.normalize("NFKD", texto)
+    texto = texto.encode("ascii", "ignore").decode("utf-8")
+
+    # Eliminar signos de puntuación
+    texto = re.sub(r"[^\w\s]", " ", texto)
+
+    # Eliminar espacios múltiples
+    texto = re.sub(r"\s+", " ", texto).strip()
+
+    return texto
